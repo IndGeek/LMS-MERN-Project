7e838a5aefe89168c85973458500bcdcd82db734
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-10-25 14:33:33 +0530

    login register page init, gitignore added

---
+++ client/pages/login.js
@@ -0,0 +1,8 @@
+const Login = () =>{
+    return(
+        <>
+            <h1 className="jumbotron text-center bg-primary square"> Login</h1>
+        </>
+    )
+}
+export default Login;
---
+++ client/pages/register.js
@@ -0,0 +1,8 @@
+const Register = () =>{
+    return(
+        <>
+            <h1 className="jumbotron text-center bg-primary square"> Register</h1>
+        </>
+    )
+}
+export default Register;
