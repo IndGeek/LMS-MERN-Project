390265bc42706d6ba0b497ea476a39ddfa1eaf3f
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-10-25 14:28:23 +0530

    new changes

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,6 +2,7 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'antd/dist/antd.css'
+import '../public/css/styles.css'
 
 function MyApp ({Component, pageProps}) {
     return <Component {...pageProps} />;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 const Index = () => {
     return(
         <>
-            <h1 className="jumbotron text-center bg-primary" >Hi there</h1>
+            <h1 className="jumbotron text-center bg-primary round-corner" >Hi there</h1>
             <p>From React and NextJS</p>
         </>
     );
